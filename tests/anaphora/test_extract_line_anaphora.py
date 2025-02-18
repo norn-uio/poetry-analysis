ddedd06ba5d36dd396c3a2fb4b26df4bb9e67590
Ingerid Dale <ingerid.dale@nb.no>
2025-02-18 13:51:46 +0100

    tests: add tests to check for empty outputs

--- tests/anaphora/test_extract_line_anaphora.py
+++ tests/anaphora/test_extract_line_anaphora.py
@@ -1,7 +1,7 @@
 from poetry_analysis.anaphora import extract_line_anaphora
 
 
-def test_extract_line_anaphora():
+def test_extract_line_anaphora_extracts_longest_repeating_sequence():
     text = "hello hello world\nhello world hello world hello world"
     expected = [{
         "line":0,
@@ -14,3 +14,26 @@ def test_extract_line_anaphora():
     }]
     result = extract_line_anaphora(text)
     assert result == expected
+
+
+def test_line_final_word_sequences_return_empty_list():
+    text = (
+        "Hei hello world hello world" + "\n"
+        "Hey hello hello" + "\n\n"
+        "hallo world world"+ "\n"
+        "Ai hello world"
+    )
+    result = extract_line_anaphora(text)
+    assert result == []
+
+
+def test_lines_without_anaphora_returns_empty_list():
+    text = (
+        "hello world" + "\n"
+        "here we are" + "\n"
+        "\n"
+        "nothing to find here" + "\n"
+        "only words that don't repeat"
+    )
+    result = extract_line_anaphora(text)
+    assert result == []
