b81ad5540cf4e3751a3d0931927a475716215882
Ingerid Dale <ingerid.dale@nb.no>
2025-04-02 20:31:01 +0200

    test shared ending works for transcriptions

--- tests/rhyme_detection/test_shared_ending_substring.py
+++ tests/rhyme_detection/test_shared_ending_substring.py
@@ -1,3 +1,4 @@
+import pytest
 from poetry_analysis import rhyme_detection as rd
 
 
@@ -8,6 +9,7 @@ def test_shared_ending_substring():
     result = rd.shared_ending_substring(string1, string2)
     
     assert result == "uld"
+
 
 def test_shared_ending_substring_no_match():
     string1 = "skamfullt"
@@ -26,3 +28,33 @@ def test_shared_ending_substring_transcription():
     
     assert result == "OO D"
 
+
+@pytest.mark.parametrize(
+    "sequence1, sequence2",
+    [
+        ("klangen", "klang"),
+        ("klang", "klangen"),
+        ("arbeider", "arbeidene"),
+        ("klangen", "sang"),
+        ("sang", "klangen"),
+    ],
+)
+def test_no_shared_ending_substring_is_found(sequence1, sequence2):
+    result = rd.shared_ending_substring(sequence1, sequence2)
+    assert not result
+
+
+@pytest.mark.parametrize(
+    "sequence1, sequence2",
+    [
+        (["F", "R", "YY1", "D"], ["S", "YY1", "D"]),
+        (["S", "II1", "N"], ["D", "II2", "N"]),
+        ("F R YY D", "S YY D"),
+        ("S II N", "D II N"),
+        ("G UH L", "J UH L"),
+        ("G UH2 L", "F UH2 L"),
+        ("B OO D", "S T OO D"),
+])
+def test_shared_ending_substring_phonemic(sequence1, sequence2):
+    result = rd.shared_ending_substring(sequence1, sequence2)
+    assert result
